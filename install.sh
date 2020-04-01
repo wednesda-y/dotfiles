d80038abf277849efa683976e3402d3f3a204f74
Darren Xuan <darrenx@student.unimelb.edu.au>
2020-04-01 15:19:22 +1100

    + added install script for strawberry

---
+++ install.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+dotfile_dir=pwd;
+
+ln -sf dotfile_dir/.config/bspwm $HOME/.config/bspwr
+ln -sf dotfile_dir/.config/sxhkd $HOME/.config/sxhkd
+ln -sf dotfile_dir/.config/nvim $HOME/.config/nvim
+ln -sf dotfile_dir/.cache/wal $HOME/.cache/wal
+ln -sf dotfile_dir/.xinitrc $HOME/.xinitrc
+ln -sf dotfile_dir/.Xresources $HOME/.Xresources
+ln -sf dotfile_dir/.zshrc $HOME/.zshrc
