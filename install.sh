0a5ca5fb549577f9c384e34e518816dfcdca0720
Darren Xuan <darrenx@student.unimelb.edu.au>
2020-04-03 16:43:50 +1100

    + added dunstrc to install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,4 @@ ln -sf dotfile_dir/.cache/wal $HOME/.cache/wal
 ln -sf dotfile_dir/.xinitrc $HOME/.xinitrc
 ln -sf dotfile_dir/.Xresources $HOME/.Xresources
 ln -sf dotfile_dir/.zshrc $HOME/.zshrc
+ln -sf dotfile_dir/.config/dunst $HOME/.config/dunst
